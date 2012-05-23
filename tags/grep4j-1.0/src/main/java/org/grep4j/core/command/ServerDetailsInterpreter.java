ba380e9f79d93316400a6c87a47cf79f93a71439
marcocast@gmail.com <marcocast@gmail.com@8edb2d6a-82ce-92ff-7885-16c997877364>
2012-05-23 16:20:44 +0000

    [maven-release-plugin]  copy for tag grep4j-1.0

---
+++ tags/grep4j-1.0/src/main/java/org/grep4j/core/command/ServerDetailsInterpreter.java
@@ -0,0 +1,28 @@
+package org.grep4j.core.command;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.grep4j.core.command.linux.CommandExecutor;
+import org.grep4j.core.command.linux.LocalCommandExecutor;
+import org.grep4j.core.command.linux.SshCommandExecutor;
+import org.grep4j.core.profile.model.ServerDetails;
+
+/**
+ * Class used to understand if a command has to be run in remote/local and possibly in Linux or windows based on the server Details 
+ * @author marcocast
+ *
+ */
+public class ServerDetailsInterpreter {
+	private static final List<String> localhostAliases = Arrays.asList("localhost","127.0.0.1"); 
+
+	public static CommandExecutor getCommandExecutor(ServerDetails serverDetails){
+		CommandExecutor commandExecutor = null;
+		if(localhostAliases.contains(serverDetails.getHost().toLowerCase())){
+			commandExecutor = new LocalCommandExecutor(serverDetails);
+		}else{
+			commandExecutor = new SshCommandExecutor(serverDetails);
+		}
+		return commandExecutor;
+	}	
+}
