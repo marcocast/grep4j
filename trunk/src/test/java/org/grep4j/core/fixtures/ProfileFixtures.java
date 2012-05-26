07a513accbc4b6ba75840eaa7ee2e3ac6bd323a6
marcocast <marcocast@8edb2d6a-82ce-92ff-7885-16c997877364>
2012-05-26 18:28:35 +0000

    operative system detector added

--- trunk/src/test/java/org/grep4j/core/fixtures/ProfileFixtures.java
+++ trunk/src/test/java/org/grep4j/core/fixtures/ProfileFixtures.java
@@ -1,8 +1,10 @@
 package org.grep4j.core.fixtures;
 
+import static org.grep4j.core.command.OperativeSystemDetector.isWindows;
+import static org.grep4j.core.fixtures.ServerDetailsFixtures.localhostServerDetails;
+
 import java.net.URL;
 
-import static org.grep4j.core.fixtures.ServerDetailsFixtures.localhostServerDetails;
 import org.grep4j.core.model.Profile;
 
 public class ProfileFixtures {
@@ -12,8 +14,11 @@ public class ProfileFixtures {
 
 	public static Profile localProfile() {
 		String fileName = "local.txt";
-		URL url = ProfileFixtures.class.getClassLoader().getResource("local.txt");
+		URL url = ProfileFixtures.class.getClassLoader().getResource(fileName);
 		String resourcePath = url.getPath();
+		if (isWindows()) {
+			resourcePath = resourcePath.replaceAll(":", "");
+		}
 		Profile profile = new Profile();
 		profile.setName("local");
 		profile.setFileLocation(resourcePath.replaceAll(fileName, ""));
