c2c5dccdbd97862d970b92f3f2ec717aa814d660
marcocast@gmail.com <marcocast@gmail.com@8edb2d6a-82ce-92ff-7885-16c997877364>
2012-07-06 16:29:33 +0000

    [maven-release-plugin]  copy for tag grep4j-1.8

---
+++ tags/grep4j-1.8/src/test/java/org/grep4j/core/command/linux/SshCommandExecutorTest.java
@@ -0,0 +1,102 @@
+package org.grep4j.core.command.linux;
+
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
+import java.io.ByteArrayInputStream;
+
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
+
+import org.easymock.EasyMock;
+import org.grep4j.core.command.ExecutableCommand;
+import org.grep4j.core.fixtures.ServerDetailsFixtures;
+import org.grep4j.core.model.ServerDetails;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ * @author ggargiulo
+ * 
+ */
+@Test(enabled=false)
+public class SshCommandExecutorTest {
+
+	private static String COMMAND_TO_EXECUTE = "command to execute";
+
+	private SSHClient sshClient;
+
+	private ServerDetails serverDetails;
+
+	private SshCommandExecutor executor;
+
+	private Session session;
+
+	private Command command;
+
+	public void testInit() throws Exception {
+
+		sshClient = EasyMock.createStrictMock(SSHClient.class);
+
+		serverDetails = ServerDetailsFixtures.aDummyRemoteServerDetails();
+
+		executor = SshCommandExecutor.aCustomSshCommandExecutor(serverDetails,
+				sshClient);
+
+		sshClient.addHostKeyVerifier(anyObject(PromiscuousVerifier.class));
+		sshClient.connect(serverDetails.getHost());
+		sshClient.authPassword(serverDetails.getUser(),
+				serverDetails.getPassword());
+
+		replay(sshClient);
+
+		executor.init();
+
+		verify(sshClient);
+
+	}
+
+	public void testExecute() throws Exception {
+
+		sshClient = EasyMock.createNiceMock(SSHClient.class);
+		session = EasyMock.createNiceMock(Session.class);
+		command = EasyMock.createNiceMock(Command.class);
+
+		serverDetails = ServerDetailsFixtures.aDummyRemoteServerDetails();
+
+		executor = SshCommandExecutor.aCustomSshCommandExecutor(serverDetails,
+				sshClient);
+
+		expect(sshClient.startSession()).andReturn(session);
+		expect(session.exec(eq(COMMAND_TO_EXECUTE))).andReturn(command);
+		expect(command.getInputStream()).andReturn(
+				new ByteArrayInputStream("Whatever".getBytes()));
+
+		// Command cmd = session.exec(command.getCommandToExecute());
+		// result = IOUtils.readFully(cmd.getInputStream()).toString();
+		// cmd.join(5, TimeUnit.SECONDS);
+
+		replay(sshClient);
+
+		executor.execute(new ExecutableCommand() {
+			@Override
+			public String getCommandToExecute() {
+				return COMMAND_TO_EXECUTE;
+			}
+		});
+
+		verify(sshClient);
+	}
+
+	//
+	//
+	// public void testQuit() {
+	// executor.quit();
+	// }
+
+}
