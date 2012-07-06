e93b3c6e633ca51794b33706f06b90eb2f307b08
marcocast@gmail.com <marcocast@gmail.com@8edb2d6a-82ce-92ff-7885-16c997877364>
2012-07-06 14:54:05 +0000

    [maven-release-plugin]  copy for tag grep4j-1.7

---
+++ tags/grep4j-1.7/src/main/java/org/grep4j/core/options/Option.java
@@ -0,0 +1,50 @@
+package org.grep4j.core.options;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+public class Option {
+
+	private final String optionCommand;
+	private final String optionValue;
+
+	public Option(String optionCommand, String optionValue) {
+		this.optionCommand = optionCommand;
+		this.optionValue = optionValue;
+	}
+
+	public Option(String optionCommand) {
+		this.optionCommand = optionCommand;
+		this.optionValue = null;
+	}
+
+	public String getOptionCommand() {
+		return optionCommand;
+	}
+
+	public String getOptionValue() {
+		return optionValue;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(optionCommand);
+		if (optionValue != null) {
+			stringBuilder.append(" ");
+			stringBuilder.append(optionValue);
+		}
+		return stringBuilder.toString();
+	}
+
+}
---
+++ tags/grep4j-1.7/src/test/java/org/grep4j/core/matchers/GrepResultContains.java
@@ -0,0 +1,32 @@
+package org.grep4j.core.matchers;
+
+import org.grep4j.core.result.GrepResultsSet;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class GrepResultContains extends TypeSafeMatcher<GrepResultsSet> {
+
+	private final String expression;
+
+	private GrepResultContains(String expression) {
+		this.expression = expression;
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		description.appendText("does not contain expression : " + expression);
+	}
+
+	@Override
+	public boolean matchesSafely(GrepResultsSet results) {
+		return results.totalOccurrences(expression) > 0;
+	}
+
+	@Factory
+	public static <T> Matcher<GrepResultsSet> containsExpression(
+			String expression) {
+		return new GrepResultContains(expression);
+	}
+}
