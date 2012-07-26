69a34271aed59f52e170335c89016a7ee3c39f35
marcocast@gmail.com <marcocast@gmail.com@8edb2d6a-82ce-92ff-7885-16c997877364>
2012-07-26 18:09:42 +0000

    [maven-release-plugin]  copy for tag grep4j-1.6.3

---
+++ tags/grep4j-1.6.3/src/main/java/org/grep4j/core/task/GrepRequest.java
@@ -0,0 +1,103 @@
+package org.grep4j.core.task;
+
+import static ch.lambdaj.Lambda.join;
+
+import java.util.List;
+
+import org.grep4j.core.model.Profile;
+import org.grep4j.core.model.ServerDetails;
+import org.grep4j.core.options.Option;
+
+/**
+ * Class container of the grep request
+ * 
+ * @author Marco Castigliego
+ * @author Giovanni Gargiulo
+ *
+ */
+public class GrepRequest {
+
+	private static final String SPACE = " ";
+
+	protected final Profile profile;
+
+	private final String expression;
+	private final boolean isRegexExpression;
+	private String contextControls;
+
+	/**
+	 * @param pattern search 
+	 * @param profile to grep
+	 * @param isRegexExpression
+	 */
+	public GrepRequest(String expression, Profile profile, boolean isRegexExpression) {
+		this.profile = profile;
+		this.expression = expression;
+		this.isRegexExpression = isRegexExpression;
+	}
+
+	/**
+	 * @param pattern search 
+	 * @param profile to grep
+	 */
+	public GrepRequest(String pattern, Profile profile) {
+		this.profile = profile;
+		this.expression = pattern;
+		this.isRegexExpression = false;
+	}
+
+	/**
+	 * Add Extra options {@see Option}
+	 * @param options
+	 */
+	public void addOptions(List<Option> options) {
+		this.contextControls = join(options, SPACE);
+	}
+
+	/**
+	 * @return the Context Controls grouped as string
+	 */
+	public String getContextControls() {
+		return contextControls;
+	}
+
+	/**
+	 * 
+	 * @return if the expression is a regex expression
+	 */
+	public boolean isRegexExpression() {
+		return this.isRegexExpression;
+	}
+	
+	/**
+	 * 
+	 * @return a copy of this GrepRequest with isRegexExpression set to true
+	 */
+	public GrepRequest copyWithRegEx() {
+		return new GrepRequest(this.getExpression(), this.getProfile(),true);
+	}
+	
+	/**
+	 * 
+	 * @return a copy of this GrepRequest with isRegexExpression set to false
+	 */
+	public GrepRequest copyWithNoRegEx() {
+		return new GrepRequest(this.getExpression(), this.getProfile(),false);
+	}
+
+	/**
+	 * @return the pattern / exression to search
+	 */
+	public String getExpression() {
+		return expression;
+	}
+
+	public ServerDetails getServerDetails() {
+		return profile.getServerDetails();
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+}
