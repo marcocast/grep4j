5fca2c5cfee46af22fe42aac8953891c64dcfa90
marcocast@gmail.com <marcocast@gmail.com@8edb2d6a-82ce-92ff-7885-16c997877364>
2012-12-14 10:55:19 +0000

    Removed unused imports

--- trunk/src/main/java/org/grep4j/core/result/GrepResults.java
+++ trunk/src/main/java/org/grep4j/core/result/GrepResults.java
@@ -1,8 +1,6 @@
 package org.grep4j.core.result;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
